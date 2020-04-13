2d9ee065083bf402914354e202cdfd90ea2e448d
WhitePaperKat <lostfile14@aol.com>
2020-04-13 19:32:23 -0400

    Update board.sh
    added time stamp

--- board.sh
+++ board.sh
@@ -12,6 +12,7 @@ if [ $chum == 1 ]
  echo "message hit enter for new line press ~ when done"
  read -r -d '~' me
  echo "---------------------------" >> Threads.log
+ echo "Time: $(date)" >> Threads.txt
  echo "Topic: $to" >> Threads.txt
  echo "$me" >> Threads.txt
  echo "poof it is done"

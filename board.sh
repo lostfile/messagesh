d92dccefd846b803eff5d1f2a2d063bf4d8d94ce
lostfile <lostfile14@aol.com>
2020-04-13 19:08:08 -0400

    Create board.sh

---
+++ board.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo message board script v1.0
+echo 1. post
+echo 2. view
+echo -n "numb:"
+read chum
+
+if [ $chum == 1 ]
+ then
+ echo -n "Topic:"
+ read to
+ echo "message hit enter for new line press ~ when done"
+ read -r -d '~' me
+ echo "---------------------------" >> Threads.log
+ echo "Topic: $to" >> Threads.txt
+ echo "$me" >> Threads.txt
+fi
+
+if [ $chum == 2 ]
+ then
+ cat Threads.txt
+fi

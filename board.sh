9e8480035bb9e150dc4cdd0ab9c5047c453924e7
WhitePaperKat <lostfile14@aol.com>
2020-04-13 19:47:07 -0400

    Update board.sh
    add the ability to download threads.txt using the xmodem protocol may require lrzsz to be installed on the server end

--- board.sh
+++ board.sh
@@ -2,6 +2,7 @@
 echo message board script v1.0
 echo 1. post
 echo 2. view
+echo 3. download thread.txt you need teraterm
 echo -n "numb:"
 read chum
 
@@ -20,4 +21,9 @@ fi
 if [ $chum == 2 ]
  then
  cat Threads.txt
+fi
+
+if [ $chum == 3 ]
+ then
+ sx Threads.txt
 fi

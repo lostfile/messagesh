f095a1c6f72020f80da1c14b99a10ab21c84fef9
WhitePaperKat <lostfile14@aol.com>
2020-04-16 15:48:32 -0400

    Update board.sh
    rewrite

--- board.sh
+++ board.sh
@@ -1,36 +1,6 @@
 #!/bin/bash
 cat motd.txt
-entries=( "post" "view" "download")
-echo -n "numb:"
-read chum
-
-if [ $chum == 1 ]
- then
- echo -n "Topic:"
- read to
- echo "message hit enter for new line press ~ when done"
- read -r -d '~' me
- echo "---------------------------" >> Threads.txt
- echo "Time: $(date)" >> Threads.txt
- echo "Topic: $to" >> Threads.txt
- echo "$me" >> Threads.txt
-fi
-
-if [ $chum == 2 ]
- then
- cat Threads.txt
- echo -n "go back to menu"
- read beert
-fi 
-
-if [ $beert == "n" ]
-then
-EOF
-else
-
-fi
-
-if [ $chum == 3 ]
- then
- sx Threads.txt
-fi
+echo TextBoard V1.1
+OPTIONS=(1 "post"
+         2 "view"
+         3 "download")

db188fea4c277f8c087a80a1cf8bce7f8e8654cc
WhitePaperKat <lostfile14@aol.com>
2020-04-16 16:17:58 -0400

    Update board.sh

--- board.sh
+++ board.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 cat motd.txt
-echo TextBoard V1.1
-do
-while [boo = 0]
-
-    read opi
-    case "$opi" in
-    1)
-    echo -n "Topic:"
-    read topic
-    echo $topic >> thread.txt
-    echo msg
-    read -d 'END' mess
-    echo mess >> thread.txt
-    boo = 1
-    ;;
-    2)
-    
-    done
+echo Menu
+echo 1 post
+echo 2 view 
+echo 3 download

a15d06571423b641960835e19fec77177b67ae31
WhitePaperKat <lostfile14@aol.com>
2020-04-17 08:48:11 -0400

    Update board.sh

--- board.sh
+++ board.sh
@@ -15,11 +15,12 @@ case $beans in
 1)
 echo -n "Topic:"
 read topc
-echo "--------------------------------" >> threads.txt
+echo "-----------------------------------------" >> threads.txt
 echo "Time: $(date)" >> threads.txt
 echo "Topic: $topc" >> threads.txt
 echo "body hit ~ to stop"
 read -r -d '~' bock
+echo"===========================================" >> threads.txt
 echo $bock >> threads.txt
 ;;
 2)

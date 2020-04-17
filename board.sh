dc5882bf873a5a45c0439568ff986ca51a9d445b
WhitePaperKat <lostfile14@aol.com>
2020-04-17 08:56:42 -0400

    Update board.sh
    added a space

--- board.sh
+++ board.sh
@@ -17,6 +17,7 @@ echo -n "Topic: "
 read topc
 echo "-----------------------------------------" >> threads.txt
 echo "Time: $(date)" >> threads.txt
+echo " " >> threads.txt
 echo "Topic: $topc" >> threads.txt
 echo "body hit ~ to stop"
 read -r -d '~' bock
